92246818dbe5e413b6f310fd31d87ed1e79f630e
DhruvinSoni30 <dhruvinsoni30@gmail.com>
2021-07-06 03:08:59 +0530

    Adding code

---
+++ p7-ifor.py
@@ -0,0 +1,7 @@
+is_high_income = True
+has_good__credit = False
+
+if is_high_income or has_good__credit:
+    print ("Eligible for loan")
+else:
+    print ("Not eligible for loan")
---
+++ p8-ifnot.py
@@ -0,0 +1,7 @@
+has_good_credit = True
+criminal_record = True
+
+if has_good_credit and not criminal_record:
+    print ("Eligible for loan")
+else:
+    print ("Not eligible for loan")
