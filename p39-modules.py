db0513195ae4cd34bb8c9e1a092c757073a224aa
DhruvinSoni30 <dhruvinsoni30@gmail.com>
2021-07-08 05:10:06 +0530

    Adding code

---
+++ p39-modules.py
@@ -0,0 +1,4 @@
+import convertors
+from convertors import kg_to_lbs
+
+print(kg_to_lbs(70))
---
+++ p40-max.py
@@ -0,0 +1,5 @@
+from utils import find_max
+
+numbers = [10,20,30,40,50]
+max =  find_max(numbers)
+print(max)
